60495c4135651fbc09e5a6e96dab3265994c68e7
luxidea <contact@luxidea.net>
2017-10-26 04:16:30 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,46 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  LUX.GPU.OpenGL.Viewer;
+
+type
+  TForm1 = class(TForm)
+    GLViewer1: TGLViewer;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     /////
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+     /////
+end;
+
+//------------------------------------------------------------------------------
+
+end. //######################################################################### ■
